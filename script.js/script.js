26b44114dfd004b7f28f40e088d98ca3407c59c7
Matheus Raposo <123lampiao10@gmail.com>
2022-08-25 18:32:49 -0300

    Finalizado projetoEstagio

--- script.js/script.js
+++ script.js/script.js
@@ -1,16 +1,11 @@
  const tbody = document.querySelector("#tbody")
-
+ 
  async function getData(){
      const url = "https://restcountries.com/v3.1/all"
      try {
          const resultado = await axios.get(url) 
-         //console.log(resultado.data)
+        
          for(elementos of resultado.data){
-            console.log(elementos)
-            //console.log(elementos.name.common)// Retorna os nomes do países
-            //console.log(elementos.capital[0]) // Retorna as capitais dos países
-            //console.log(elementos.area) // Retorna a Área dos países
-            //console.log(Object.keys(elementos.currencies)) // Retorna o tipo de moeda , OBject.keys retorna chave do objeto 
             const tr = document.createElement("tr")
             const tdName = document.createElement("td") 
             const tdCapital = document.createElement("td") 
@@ -19,12 +14,13 @@
 
 
             tdName.textContent = elementos.name.common
-            tdCapital.textContent = elementos.capital[0]
-            tdArea.textContent = elementos.area
-            tdCourrencies.textContent = Object.keys(elementos.currencies)
+            tdCapital.textContent = elementos.capital ? elementos.capital[0] : "-"
+            tdArea.textContent = elementos.area.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+            tdCourrencies.textContent = elementos.currencies ? Object.keys(elementos.currencies) : "-"
 
             tr.append(tdName,tdCapital,tdArea,tdCourrencies)
             tbody.append(tr)
+            
          }
         
     } catch (error) {
